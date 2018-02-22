4239e7cdd6c8f3a1ffefeb5163b4a3963337da4d
ZdorenkoS <zdorenkos@gmail.com>
2018-02-22 22:35:20 +0200

    Первый гит

--- 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
@@ -1,6 +1,9 @@
 package com.javarush.task.task20.task2003;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 /* 
 Знакомство с properties
@@ -8,16 +11,29 @@ import java.io.*;
 public class Solution {
     public static Map<String, String> properties = new HashMap<>();
 
-    public void fillInPropertiesMap() {
-        //implement this method - реализуйте этот метод
+    public void fillInPropertiesMap() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        FileInputStream fis = new FileInputStream(br.readLine());
+        load(fis);
     }
 
     public void save(OutputStream outputStream) throws Exception {
-        //implement this method - реализуйте этот метод
+        Properties prop = new Properties();
+        for (Map.Entry<String, String> map :properties.entrySet()) {
+            prop.setProperty(map.getKey(),map.getValue());
+        }
+        prop.store(outputStream,"");
+
     }
 
     public void load(InputStream inputStream) throws Exception {
-        //implement this method - реализуйте этот метод
+        Properties prop = new Properties();
+        prop.load(inputStream);
+        for (Map.Entry<Object, Object> map :prop.entrySet()) {
+            properties.put(map.getKey().toString(),map.getValue().toString());
+        }
+
+
     }
 
     public static void main(String[] args) {
