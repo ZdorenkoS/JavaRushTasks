b9d5e30ba4c0c29b6a67c334d8ebd9ea57c809ac
Daos <zdorenkos@gmail.com>
2018-02-25 14:30:47 +0200

    _

--- 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
@@ -1,13 +1,12 @@
 package com.javarush.task.task18.task1803;
 
-import sun.rmi.log.LogInputStream;
+
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 
 /* 
 Самые частые байты
