d07e1c08c371939b43620c0aa420146c6cac98e3
ZdorenkoS <zdorenkos@gmail.com>
2018-03-19 23:10:00 +0200

    22_10

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2210/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2210/Solution.java
@@ -1,13 +1,27 @@
 package com.javarush.task.task22.task2210;
 
-/* 
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+/*
 StringTokenizer
 */
 public class Solution {
     public static void main(String[] args) {
-
+      /* String [] s = getTokens("level22.lesson13.task01", ".");
+        for (String str : s) {
+            System.out.print(str);
+        }*/
     }
     public static String [] getTokens(String query, String delimiter) {
-        return null;
+        StringTokenizer tokenizer = new StringTokenizer(query, delimiter);
+        ArrayList<String> list = new ArrayList<>();
+        list.add("{");
+         while (tokenizer.hasMoreTokens()) {
+            list.add("\""+tokenizer.nextToken()+"\"");
+            if (tokenizer.hasMoreTokens())list.add(", ");
+        }
+        list.add("}");
+         return list.toArray(new String[list.size()]);
     }
 }
