366702d0780cb1c50f8bf1c4a03fa54b79cdc1c8
ZdorenkoS <zdorenkos@gmail.com>
2018-06-08 14:58:26 +0300

    30.08 chat

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -3,8 +3,21 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
+    public static void sendBroadcastMessage(Message message){
+        try {
+            for (Map.Entry<String, Connection> pair : connectionMap.entrySet())
+                pair.getValue().send(message);
+        } catch (IOException e) {
+            System.out.println("Сообщение не отправлено");
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = null;
         try {
@@ -29,9 +42,30 @@ public class Server {
 
     private static class Handler extends Thread{
         private Socket socket;
-
         public Handler(Socket socket) {
             this.socket = socket;
+        }
+
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException{
+           Message m;
+           String name;
+            while (true){
+                connection.send(new Message(MessageType.NAME_REQUEST));
+                m=connection.receive();
+                name = m.getData();
+                if (m.getType()==MessageType.USER_NAME && !name.isEmpty() && !connectionMap.containsKey(name)) break;
+            }
+            connectionMap.put(name,connection);
+            connection.send(new Message(MessageType.NAME_ACCEPTED));
+            return name;
+        }
+
+        private void sendListOfUsers(Connection connection, String userName) throws IOException{
+            for (Map.Entry<String, Connection> map:connectionMap.entrySet()) {
+                if (map.getKey()==userName) continue;
+                connection.send(new Message(MessageType.USER_ADDED, map.getKey()));
+
+            }
         }
     }
 }
